5d42bbe9d1351ba5e0ae7e748fc3572bd45d70d5
Andrey Poddubny <apoddubny@speroteck.com>
2014-12-03 08:04:16 -0800

    bug fixing

--- app/themes/default/scripts/scripts.js
+++ app/themes/default/scripts/scripts.js
@@ -1,19 +1,6 @@
 (function ($) {
     "use strict";
-
-    // custom scrollbar
-
-    $("html").niceScroll({styler: "fb", cursorcolor: "#65cea7", cursorwidth: '6', cursorborderradius: '0px', background: '#424f63', spacebarenabled: false, cursorborder: '0', zindex: '1000'});
-
-    $(".left-side").niceScroll({styler: "fb", cursorcolor: "#65cea7", cursorwidth: '3', cursorborderradius: '0px', background: '#424f63', spacebarenabled: false, cursorborder: '0'});
-
-
-    $(".left-side").getNiceScroll();
-    if ($('body').hasClass('left-side-collapsed')) {
-        $(".left-side").getNiceScroll().hide();
-    }
-
-
+    
     function mainContentHeightAdjust() {
         // Adjust main content height
         var docHeight = $(document).height();
