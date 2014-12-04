9159888ba012ae0a03adaef0a3b5585489df63e0
zheyakush <zhenya1708@inbox.ru>
2014-12-04 15:54:19 +0200

    Fixed sorting in edit forms

--- app/scripts/config/directives/guiConfigEditorForm.js
+++ app/scripts/config/directives/guiConfigEditorForm.js
@@ -17,7 +17,8 @@
                     },
                     templateUrl: $designService.getTemplate("config/gui/configEditorForm.html"),
                     controller: function ($scope) {
-                        var updateAttributes;
+                        var updateAttributes, addTab, addFields, sortFieldsInGroups;
+
                         $scope.tabs = {};
                         $scope.click = function (id) {
                             if (typeof $scope.parent.selectTab === "function") {
@@ -27,7 +28,7 @@
                             }
                         };
 
-                        var addTab = function(attr) {
+                        addTab = function (attr) {
                             if (attr.Type === "group") {
                                 if (typeof $scope.tabs[attr.Group] === "undefined") {
                                     $scope.tabs[attr.Group] = [];
@@ -36,11 +37,30 @@
                             }
                         };
 
-                        var addFields = function(attr) {
+                        addFields = function (attr) {
                             if (typeof $scope.attributeGroups[attr.Group] === "undefined") {
                                 $scope.attributeGroups[attr.Group] = [];
                             }
                             $scope.attributeGroups[attr.Group].push(attr);
+                        };
+
+                        sortFieldsInGroups = function () {
+                            var sortByLabel, tab;
+                            sortByLabel = function (a, b) {
+                                if (a.Label.toString() < b.Label.toString()) {
+                                    return -1;
+                                }
+                                if (a.Label.toString() > b.Label.toString()) {
+                                    return 1;
+                                }
+
+                                return 0;
+                            };
+                            for (tab in $scope.attributeGroups) {
+                                if ($scope.attributeGroups.hasOwnProperty(tab)) {
+                                    $scope.attributeGroups[tab].sort(sortByLabel);
+                                }
+                            }
                         };
 
                         updateAttributes = function () {
@@ -64,12 +84,11 @@
                                     attr = setAttrValue($scope.attributes[i]);
 
                                     addFields(attr);
-
                                     addTab(attr);
                                 }
                             }
 
-
+                            sortFieldsInGroups();
                         };
 
                         $scope.$watchCollection("attributes", updateAttributes);
