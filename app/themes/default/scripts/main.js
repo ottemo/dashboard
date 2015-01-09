cad3c5dc483b08e032ac0da1c7efc96a4c0fe137
zheyakush <zhenya1708@inbox.ru>
2015-01-09 13:34:17 +0200

    [#85661688] temporary fix this issue

--- app/themes/default/scripts/main.js
+++ app/themes/default/scripts/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // click on tab link
-	$('.nav-tabs a').click(function (e) {
-	    e.preventDefault()
-	    $(this).tab('show')
-	});
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
 
     // sho/hide filsters
-	$(document).on('click', '.filters', function() {
-        $('#filters').toggle(0)
+    $(document).on('click', '.filters', function () {
+        $('#filters').toggle(0);
     });
 
     // click on offcanvas button
-    $(document).on('click', '[data-toggle="offcanvas"]', function(event) {
+    $(document).on('click', '[data-toggle="offcanvas"]', function (event) {
         event.preventDefault();
         $(this).toggleClass('active');
         $('#offcanvas').toggleClass('active');
         $('.offcanvas-wrapper').toggleClass('active');
     });
-        
+
     //theme switcher
-    $(document).on('click', '#switcher .preview', function() {	
+    $(document).on('click', '#switcher .preview', function () {
 
         // clear the containers and storage
-    	$('#code_container').html('');
+        $('#code_container').html('');
         $('#theme_container').html('');
         localStorage.setItem("cssCode", '');
         localStorage.setItem("activeTheme", '');
@@ -33,12 +33,12 @@ $(document).ready(function() {
         var link = '<link rel="stylesheet" href="themes/default/styles/themes/' + thisId + '.css" type="text/css" />';
 
         // set theme and storage
-    	$('#theme_container').append(link);
+        $('#theme_container').append(link);
         localStorage.setItem("activeTheme", link);
     });
 
     // apply the code
-    $(document).on('click', '#apply', function() {
+    $(document).on('click', '#apply', function () {
 
         // clear the containers and storage
         $('#code_container').html('');
@@ -59,38 +59,38 @@ $(document).ready(function() {
     document.getElementById("theme_container").innerHTML = localStorage.getItem("activeTheme");
     var navbarTop = localStorage.getItem('navbarTop');
 
-    setTimeout(function(){
+    setTimeout(function () {
         //nice scroll
         $("#offcanvas").niceScroll();
         $("#switcher").niceScroll();
 
         // set the theme
-        $('#switcher .preview').each(function() {
+        $('#switcher .preview').each(function () {
             var thisId = $(this).attr('id');
             $(this).children('img').attr('src', 'themes/default/images/themes/' + thisId + '.jpg');
         });
 
         //set sidebar position
-        if ( navbarTop == '1' ) {
+        if (navbarTop === '1') {
             $('#header_sidebar').css('display', 'none');
             $('#header_no_sidebar').css('display', 'block');
             $('#offcanvas').removeClass('active');
             $('body').removeClass('sticky-sidebar');
-        } 
+        }
         else {
             $('#header_no_sidebar').css('display', 'none');
             $('body').css('paddingTop', 0).removeClass('have-fixed-sidebar');
             $('#header_no_sidebar').removeClass('navbar-fixed-top');
             $('#header_sidebar').css('display', 'block');
-        };
-    },500)
+        }
+    }, 2500);
 
 
-    $(document).on('click', '#switcher-toogle', function() {
+    $(document).on('click', '#switcher-toogle', function () {
         $('#theme_switcher > span.btn').toggleClass('active');
-        if ( ($('#theme_switcher').hasClass('active')) == true ) {
-            $('#theme_switcher').removeClass('active')
-        };
+        if (($('#theme_switcher').hasClass('active')) === true) {
+            $('#theme_switcher').removeClass('active');
+        }
     });
 
     // clickable scroll bottom
@@ -98,8 +98,8 @@ $(document).ready(function() {
         var step = 150;
         $("#switcher").stop().animate({
             scrollTop: "+=" + step + "px"
-        },400);
-         event.preventDefault();
+        }, 400);
+        event.preventDefault();
     });
 
     // clickable scroll top
@@ -107,33 +107,33 @@ $(document).ready(function() {
         var step = 150;
         $("#switcher").stop().animate({
             scrollTop: "-=" + step + "px"
-        },400);
-         event.preventDefault();
+        }, 400);
+        event.preventDefault();
     });
 
     // toggle theme_switcher
-    $(document).on('click', '#theme_switcher > span.btn', function() {
+    $(document).on('click', '#theme_switcher > span.btn', function () {
         $('#theme_switcher').toggleClass('active');
     });
 
     // uploading files
-    $(document).on('click', '.upl-btn', function() {
+    $(document).on('click', '.upl-btn', function () {
         $(this).prev('.upl-input').trigger('click');
     });
 
     // modals table checkbox active on td click
-    $(document).on('click', '.modal-dialog table tbody td', function() {
+    $(document).on('click', '.modal-dialog table tbody td', function () {
         $(this).parents('tr').children('td:first-child').children('input').trigger('click');
     });
 
     // click on element of offcanvas
-    $(document).on('click', '#offcanvas a.menu-item', function(event) {
+    $(document).on('click', '#offcanvas a.menu-item', function (event) {
         var itHasLink = $(this).attr('href');
 
-        if ( itHasLink == '' ) {
+        if (itHasLink === '') {
             event.preventDefault();
-            $('#offcanvas .sub-menu').not($(this).next()).slideUp(300)
-            $(this).next('.sub-menu').slideToggle(300);  
+            $('#offcanvas .sub-menu').not($(this).next()).slideUp(300);
+            $(this).next('.sub-menu').slideToggle(300);
         }
         else {
             $('#offcanvas').removeClass('active');
@@ -144,16 +144,16 @@ $(document).ready(function() {
 
     //navtop function
     function goTopNavbarMode() {
-        $('#header_sidebar').fadeOut(500,function(){
+        $('#header_sidebar').fadeOut(500, function () {
             $('#header_no_sidebar').fadeIn(500);
             $('#offcanvas').removeClass('active');
             $('body').removeClass('sticky-sidebar');
         });
-        
-    };
+
+    }
 
     // go to 'no_sidebar' mode
-    $(document).on('click', '#navbar_switch_top', function(event) {
+    $(document).on('click', '#navbar_switch_top', function () {
         goTopNavbarMode();
         localStorage.removeItem('navbarTop');
         localStorage.setItem('navbarTop', '1');
@@ -161,37 +161,37 @@ $(document).ready(function() {
 
     //sidebar function
     function goSideBarMode() {
-        $('#header_no_sidebar').fadeOut(500,function(){
+        $('#header_no_sidebar').fadeOut(500, function () {
             $('body').css('paddingTop', 0).removeClass('have-fixed-sidebar');
             $('#header_no_sidebar').removeClass('navbar-fixed-top');
             $('#header_sidebar').fadeIn(500);
             $('#offcanvas').addClass('active');
         });
-        
+
     }
 
     // go to 'sidebar' mode
-    $(document).on('click', '#navbar_switch_side', function (){
+    $(document).on('click', '#navbar_switch_side', function () {
         goSideBarMode();
         localStorage.removeItem('navbarTop');
         localStorage.setItem('navbarTop', '0');
     });
 
     // fix the navbar in the top
-    $(document).on('click', '#fix_navbar', function() {
+    $(document).on('click', '#fix_navbar', function () {
         var navbarFixed = $('#header_no_sidebar').hasClass('navbar-fixed-top');
-        if ( navbarFixed == true ) {
-            $('body').animate({paddingTop:0}, 400);
+        if (navbarFixed === true) {
+            $('body').animate({paddingTop: 0}, 400);
         }
         else {
             var navbarHeight = $('#header_no_sidebar').outerHeight(true);
-            $('body').animate({paddingTop:navbarHeight}, 400);
+            $('body').animate({paddingTop: navbarHeight}, 400);
         }
         $('#header_no_sidebar').toggleClass('navbar-fixed-top');
     });
 
     //make the sidebar sticky
-    $(document).on('click', '#static_sidebar', function(event) {
+    $(document).on('click', '#static_sidebar', function (event) {
         event.preventDefault();
         $('body').toggleClass('sticky-sidebar');
     });
