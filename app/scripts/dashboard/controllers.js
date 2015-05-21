a4f17f5bf776779ad1fe8e7402ea4e31462f752f
Adam Knox <adam.w.knox@gmail.com>
2015-05-21 12:35:34 -0400

    [#95049526] consolidate controller logic
    * only declare graph options once
    * plot 1 days worth of details
    * no date switching options

--- app/scripts/dashboard/controllers.js
+++ app/scripts/dashboard/controllers.js
@@ -20,6 +20,8 @@ $scope.items = $dashboardSidebarService.getItems();
 "$designImageService",
 "$dashboardUtilsService",
 function ($scope, $location, $statistic, $designImageService, $dashboardUtilsService) {
+    $scope.visitorsChartData = [];
+    $scope.salesChartData = [];
 
     var renderVisitsChart = function (data) {
         if ($scope.visitorsCharts) {
@@ -36,8 +38,6 @@ function ($scope, $location, $statistic, $designImageService, $dashboardUtilsSer
             $scope.salesCharts.draw();
         }
     };
-
-
 
     // TOP REFERRERS
     $statistic.getReferrers().then(function (data) {
@@ -65,62 +65,48 @@ function ($scope, $location, $statistic, $designImageService, $dashboardUtilsSer
     });
 
     // VISITORS ONLINE
-    $statistic.getVisitorsOnline().then(function (data) {
-        $scope.visitorsOnline = data;
-    });
+    // $statistic.getVisitorsOnline().then(function (data) {
+    //     $scope.visitorsOnline = data;
+    // });
+
+    var plotSettings = {
+        series: {
+            lines: {
+                show: true,
+                fill: false
+            },
+            shadowSize: 0
+        },
+        grid: {
+            hoverable: true,
+            clickable: true,
+            tickColor: "#f9f9f9",
+            borderWidth: 1,
+            borderColor: "#eeeeee"
+        },
+        colors: ["#65CEA7", "#424F63"],
+        xaxis: {
+            mode: "time",
+            timeformat: "%I%p"
+            // tickLength: 5,
+            // timezone: "browser" // "browser" for local to the client or timezone for timezone-js
+        },
+        yaxis: {
+            min: 0
+        }
+    }
 
     $scope.initVisitorsChart = function () {
         if (!$scope.visitorsCharts) {
             $scope.visitorsCharts = $.plot(
-                $('#visitors-chart #visitors-container'), [
-                    {
-                        data: $scope.visitorsChartData,
-                        label: "Page View",
-                        lines: {
-                            fill: true
-                        }
-                    }
-                ],
-                {
-                    series: {
-                        lines: {
-                            show: true,
-                            fill: false
-                        },
-                        points: {
-                            show: true,
-                            lineWidth: 2,
-                            fill: true,
-                            fillColor: "#ffffff",
-                            symbol: "circle",
-                            radius: 5
-                        },
-                        shadowSize: 0
-                    },
-                    grid: {
-                        hoverable: true,
-                        clickable: true,
-                        tickColor: "#f9f9f9",
-                        borderWidth: 1,
-                        borderColor: "#eeeeee"
-                    },
-                    colors: ["#65CEA7", "#424F63"],
-                    tooltip: true,
-                    tooltipOpts: {
-                        defaultTheme: false
-                    },
-                    xaxis: {
-                        mode:   "time",
-                        tickLength: 5,
-                        timezone: "browser" // "browser" for local to the client or timezone for timezone-js
-                    }
-                }
+                $('#visitors-chart #visitors-container'),
+                [{'label': 'visits', data: $scope.visitorsChartData}],
+                plotSettings
             );
         }
     };
 
     // VISITORS CHART
-    // BY DEFAULT LAST 7 DAYS
     (function () {
         var from, to, today, dd, mm, yyyy, month,  tz;
 
@@ -151,55 +137,14 @@ function ($scope, $location, $statistic, $designImageService, $dashboardUtilsSer
     $scope.initSalesChart = function () {
         if (!$scope.salesCharts) {
             $scope.salesCharts = $.plot(
-                $('#sales-chart #sales-container'), [
-                    {
-                        data: $scope.salesChartData,
-                        label: "Page View",
-                        lines: {
-                            fill: true
-                        }
-                    }
-                ],
-                {
-                    series: {
-                        lines: {
-                            show: true,
-                            fill: false
-                        },
-                        points: {
-                            show: true,
-                            lineWidth: 2,
-                            fill: true,
-                            fillColor: "#ffffff",
-                            symbol: "circle",
-                            radius: 5
-                        },
-                        shadowSize: 0
-                    },
-                    grid: {
-                        hoverable: true,
-                        clickable: true,
-                        tickColor: "#f9f9f9",
-                        borderWidth: 1,
-                        borderColor: "#eeeeee"
-                    },
-                    colors: ["#65CEA7", "#424F63"],
-                    tooltip: true,
-                    tooltipOpts: {
-                        defaultTheme: false
-                    },
-                    xaxis: {
-                        mode:   "time",
-                        tickLength: 5,
-                        timezone: "browser" // "browser" for local to the client or timezone for timezone-js
-                    }
-                }
+                $('#sales-chart #sales-container'),
+                [{'label': 'sales', data: $scope.salesChartData}],
+                plotSettings
             );
         }
     };
 
     // SALES CHART
-    // BY DEFAULT LAST 7 DAYS
     (function () {
         var from, to, today, dd, mm, yyyy, month, tz;
 
@@ -211,7 +156,7 @@ function ($scope, $location, $statistic, $designImageService, $dashboardUtilsSer
         yyyy = today.getFullYear();
         to = yyyy + "-" + mm + "-" + dd;
 
-        today.setDate(today.getDate() - 7);
+        today.setDate(today.getDate() - 1);
         dd = today.getDate().toString().length < 2 ? '0' + today.getDate() : today.getDate();
         month = today.getMonth() + 1; //January is 0!
         mm = month.toString().length < 2 ? '0' + month : month;
@@ -226,130 +171,6 @@ function ($scope, $location, $statistic, $designImageService, $dashboardUtilsSer
             }
         );
     })();
-
-    $scope.updateVisitsChart = function (period) {
-        var from, to, today, delta, dd, mm, month, yyyy, tz;
-
-        var getDeltaValueForPeriod = function (period) {
-            var delta = {};
-
-            switch (period) {
-                case "today":
-                    $scope.visitsPeriod = 'today';
-                    delta["to"] = 1;
-                    delta["from"] = 1;
-                    break;
-                case "yesterday":
-                    $scope.visitsPeriod = 'yesterday';
-                    delta["to"] = 0;
-                    delta["from"] = 1;
-                    break;
-                case "week":
-                    $scope.visitsPeriod = 'week';
-                    delta["to"] = 1;
-                    delta["from"] = 7;
-                    break;
-                case "month":
-                    $scope.visitsPeriod = 'month';
-                    delta["to"] = 1;
-                    delta["from"] = 31;
-                    break;
-                default:
-                    $scope.visitsPeriod = 'week';
-                    delta["to"] = 1;
-                    delta["from"] = 7;
-            }
-
-            return delta;
-        };
-
-        delta = getDeltaValueForPeriod(period);
-
-        today = new Date();
-        today.setDate(today.getDate() + delta["to"]);
-        dd = today.getDate().toString().length < 2 ? '0' + today.getDate() : today.getDate();
-        month = today.getMonth() + 1; //January is 0!
-        mm = month.toString().length < 2 ? '0' + month : month;
-        yyyy = today.getFullYear();
-        to = yyyy + "-" + mm + "-" + dd;
-
-        today.setDate(today.getDate() - delta["from"]);
-        dd = today.getDate().toString().length < 2 ? '0' + today.getDate() : today.getDate();
-        month = today.getMonth() + 1; //January is 0!
-        mm = month.toString().length < 2 ? '0' + month : month;
-        yyyy = today.getFullYear();
-        from = yyyy + "-" + mm + "-" + dd;
-        tz = -today.getTimezoneOffset()/60;
-
-        $statistic.getVisitsDetail(from, to, tz).then(
-            function (data) {
-                renderVisitsChart(data);
-                $scope.visitorsChartData = data;
-            }
-        );
-    };
-
-    $scope.updateSalesChart = function (period) {
-        var from, to, today, delta, dd, mm, month, yyyy, tz;
-
-        var getDeltaValueForPeriod = function (period) {
-            var delta = {};
-
-            switch (period) {
-                case "today":
-                    $scope.salesPeriod = 'today';
-                    delta["to"] = 1;
-                    delta["from"] = 1;
-                    break;
-                case "yesterday":
-                    $scope.salesPeriod = 'yesterday';
-                    delta["to"] = 0;
-                    delta["from"] = 1;
-                    break;
-                case "week":
-                    $scope.salesPeriod = 'week';
-                    delta["to"] = 1;
-                    delta["from"] = 7;
-                    break;
-                case "month":
-                    $scope.salesPeriod = 'month';
-                    delta["to"] = 1;
-                    delta["from"] = 31;
-                    break;
-                default:
-                    $scope.salesPeriod = 'week';
-                    delta["to"] = 1;
-                    delta["from"] = 7;
-            }
-
-            return delta;
-        };
-
-        delta = getDeltaValueForPeriod(period);
-
-        today = new Date();
-        today.setDate(today.getDate() + delta["to"]);
-        dd = today.getDate().toString().length < 2 ? '0' + today.getDate() : today.getDate();
-        month = today.getMonth() + 1; //January is 0!
-        mm = month.toString().length < 2 ? '0' + month : month;
-        yyyy = today.getFullYear();
-        to = yyyy + "-" + mm + "-" + dd;
-
-        today.setDate(today.getDate() - delta["from"]);
-        dd = today.getDate().toString().length < 2 ? '0' + today.getDate() : today.getDate();
-        month = today.getMonth() + 1; //January is 0!
-        mm = month.toString().length < 2 ? '0' + month : month;
-        yyyy = today.getFullYear();
-        from = yyyy + "-" + mm + "-" + dd;
-         tz = -today.getTimezoneOffset()/60;
-
-        $statistic.getSalesDetail(from, to, tz).then(
-            function (data) {
-                renderSalesChart(data);
-                $scope.salesChartData = data;
-            }
-        );
-    };
 
     $scope.getProductImage = function (image) {
         return $designImageService.getFullImagePath("", image);
