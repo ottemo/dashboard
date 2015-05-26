485e74ae78f92a5000d9175917a80ee02bd59bc0
Adam Knox <adam.w.knox@gmail.com>
2015-05-26 12:11:31 -0400

    handle sass errors

--- gulpfile.js
+++ gulpfile.js
@@ -141,7 +141,11 @@ gulp.task('themes.scripts', function () {
 gulp.task('themes.styles', function () {
     return gulp.src(paths.themes.styles,{ base: paths.themes.base })
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass())
+        .on('error', function (error) {
+            console.error(error);
+            this.emit('end');
+        })
         .pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         // .pipe(minifyCSS({
         //     processImport: false
