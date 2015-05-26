5a51ce82f13144676167011cbaabee179651459a
Adam Knox <adam.w.knox@gmail.com>
2015-05-26 11:22:01 -0400

    correct media path

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 angular.appConfig = {
-  "general.app.foundation_url": "http://foundation.richkids.dev.ottemo.io",
-  "general.app.media_path": "//richkids.com/media/",
+  "general.app.foundation_url": "https://foundation.richkids.dev.ottemo.io",
+  "general.app.media_path": "//richkidsbrand.com/media/",
   "general.app.item_per_page": 15
 };
 
