bb46a3408e70e6aec635521215393f9619c7267c
Adam Knox <adam.w.knox@gmail.com>
2015-05-28 09:16:37 -0400

    [#94795542] correct menu order, tweak icons / padding

--- app/scripts/dashboard/service/menu.js
+++ app/scripts/dashboard/service/menu.js
@@ -1,99 +1,115 @@
 // TODO:
 // * monitor route change:
-//   * update visibility toggle for xs, and sm
-//   * add "active:true" to matching menu item
+//   * add "active:true" to matching menu item, instead of with $
 // * add "afix" class to body when menu is open, so that the body doesn't scroll
 // * let the menu scroll
 //
 angular.module("dashboardModule")
 
-  .service("$menuService", [function () {
+.service("$menuService", [function () {
 
-      return [
-        {
-          "title": "Dashboard",
-          "link": "/",
-          "icon": "fa-home"
-        }, {
-          "title": "Orders",
-          "link": "/orders",
-          "icon": "fa-list-alt"
-        }, {
-          "title": "Products",
-          "link": null,
-          "icon": "fa-tags",
-          "items": [{
-            "title": "Products",
-            "link": "/products",
-            "icon": ""
-          }, {
-            "title": "Attributes",
-            "link": "/attributes",
-            "icon": ""
-          }]
-        }, {
-          "title": "Categories",
-          "link": "/categories",
-          "icon": "fa-th-list"
-        }, {
-          "title": "CMS",
-          "link": null,
-          "icon": "fa-indent",
-          "items": [{
-            "title": "Page",
-            "link": "/cms/pages",
-            "icon": ""
-          }, {
-          //  "title": "Gallery",
-          //  "link": "/cms/gallery",
-          //  "icon": ""
-          //}, {
-            "title": "Block",
-            "link": "/cms/blocks",
-            "icon": ""
-          }]
-        }, {
-        //  "title": "Import / Export",
-        //  "link": "/impex",
-        //  "icon": "fa-exchange"
-        //}, {
-        //  "title": "URL Rewrite",
-        //  "link": "/seo",
-        //  "icon": "fa-random",
-        //}, {
-          "title": "Visitors",
-          "link": null,
-          "icon": "fa-users",
-          "items": [{
-            "title": "Attributes",
-            "link": "/v/attributes",
-            "icon": ""
-          }, {
-            "title": "Email",
-            "link": "/emails",
-            "icon": ""
-          }, {
-            "title": "Visitors",
-            "link": "/visitors",
-            "icon": ""
-          }]
-        }, {
-          "title": "Settings",
-          "link": null,
-          "icon": "fa-cogs",
-          "items": [{
-            "title": "General",
-            "link": "/settings/general",
-            "icon": ""
-          }, {
-            "title": "Payment",
-            "link": "/settings/payment",
-            "icon": ""
-          }, {
-            "title": "Shipping",
-            "link": "/settings/shipping",
-            "icon": ""
-          }]
-        }]
+	// We have these routes and pages floating around as well
+	// {
+	// 	"title": "Import / Export",
+	// 	"link": "/impex",
+	// 	"icon": "fa-exchange"
+	// }
+	// {
+	// 	"title": "URL Rewrite",
+	// 	"link": "/seo",
+	// 	"icon": "fa-random",
+	// }
+	// {
+	// 	"title": "Gallery",
+	// 	"link": "/cms/gallery",
+	// 	"icon": ""
+	// }
 
-  }]);
+	var menu = [
+		{
+			"title": "Dashboard",
+			"link": "/",
+			"icon": "fa-home"
+		},
+		{
+			"title": "Orders",
+			"link": "/orders",
+			"icon": "fa-list-alt"
+		},
+		{
+			"title": "Products",
+			"link": null,
+			"icon": "fa-tags",
+			"children": [
+				{
+					"title": "Products",
+					"link": "/products"
+				},
+				{
+					"title": "Attributes",
+					"link": "/attributes"
+				}
+			]
+		},
+		{
+			"title": "Categories",
+			"link": "/categories",
+			"icon": "fa-list"
+		},
+		{
+			"title": "Visitors",
+			"link": null,
+			"icon": "fa-users",
+			"children": [
+				{
+					"title": "Attributes",
+					"link": "/v/attributes"
+				},
+				{
+					"title": "Email",
+					"link": "/emails"
+				},
+				{
+					"title": "Visitors",
+					"link": "/visitors"
+				}
+			]
+		},
+		{
+			"title": "CMS",
+			"link": null,
+			"icon": "fa-file-text-o",
+			"children": [
+				{
+					"title": "Page",
+					"link": "/cms/pages"
+				},
+				{
+					"title": "Block",
+					"link": "/cms/blocks"
+				}
+			]
+		},
+		{
+			"title": "Settings",
+			"link": null,
+			"icon": "fa-cog",
+			"children": [
+				{
+					"title": "General",
+					"link": "/settings/general"
+				},
+				{
+					"title": "Payment",
+					"link": "/settings/payment"
+				},
+				{
+					"title": "Shipping",
+					"link": "/settings/shipping"
+				}
+			]
+		}
+	];
+
+	return menu;
+}]);
