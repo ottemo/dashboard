b2b610e3abc96d01a16bc38bbe3bda35b929e506
Adam Knox <adam.w.knox@gmail.com>
2015-06-18 12:02:00 -0400

    [#92457760] discount bugs
    * time zone offsets are saved off, so there shouldn't be any timing bugs
    now
    * type bug is fixed and standardized

--- app/scripts/discounts/service/api.js
+++ app/scripts/discounts/service/api.js
@@ -16,25 +16,54 @@ angular.module("discountsModule")
 	function ($http, REST_SERVER_URI, moment) {
 		var _url = REST_SERVER_URI + '/coupons';
 
+		var _normalizeData = function(discount) {
+			// Process the datetime, down to just a date
+			discount.since = moment(discount.since).format('YYYY-MM-DD');
+			discount.until = moment(discount.until).format('YYYY-MM-DD');
+
+			// Some helper methods
+			discount.type = discount.amount ? 'amount' : 'percent';
+			discount.isNoLimit = (discount.times == -1);
+
+			return discount;
+		}
+
+		var _processDates = function(discount) {
+			discount.since = moment(discount.since).format();
+			discount.until = moment(discount.until).format();
+			return discount;
+		}
+
+		this.defaults = {
+			noLimit: true,
+			times: -1,
+			type: 'amount'
+		};
+
 		this.one = function(id) {
 			return $http.get(_url + '/' + id).then(function(response){
-
-				//"2015-05-06T00:00:00Z" -> yyyy-mm-dd
 				var result = response.data.result;
-				result.since = moment.utc(result.since).format('YYYY-MM-DD');
-				result.until = moment.utc(result.until).format('YYYY-MM-DD');
-
-				return result;
+				return _normalizeData(result);
 			});
 		}
 
 		this.post = function(data) {
+			// processing the params can effect the source data, so we need to copy it out
+			var params = {};
+			angular.copy(data, params);
+
+			params = _processDates(params);
 			return $http.post(_url, data).then(function(response){
 				return response.data.result;
 			});
 		}
 
 		this.put = function(data) {
+			// processing the params can effect the source data, so we need to copy it out
+			var params = {};
+			angular.copy(data, params);
+
+			params = _processDates(params);
 			return $http.put(_url +'/' + data._id, data).then(function(response){
 				return response.data.result;
 			});
@@ -42,7 +71,12 @@ angular.module("discountsModule")
 
 		this.getList = function() {
 			return $http.get(_url).then(function(response){
-				return response.data.result || [];
+				var results = response.data.result || [];
+				results = results.map(function(discount) {
+					return _normalizeData(discount);
+				});
+
+				return results;
 			});
 		}
 
