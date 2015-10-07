ec971981bfefb82dd5df10b78f40ad52860006a6
Adam Knox <adam.w.knox@gmail.com>
2015-10-07 10:46:17 -0400

    formatting

--- app/scripts/design/directives/filter/guiDate.js
+++ app/scripts/design/directives/filter/guiDate.js
@@ -1,8 +1,6 @@
 angular.module("designModule")
-/**
-*  Directive used for automatic attribute editor creation
-*/
-.directive("guiFilterDate", ["$designService", function ($designService) {
+
+.directive("guiFilterDate", ["$designService", function($designService) {
     return {
         restrict: "E",
         scope: {
@@ -12,16 +10,16 @@ angular.module("designModule")
         },
         templateUrl: $designService.getTemplate("design/gui/filter/date.html"),
 
-        controller: ["$scope", function ($scope) {
+        controller: ["$scope", function($scope) {
             $scope.highInvalid = false;
             $scope.lowInvalid = false;
             $scope.low;
             $scope.high;
             var isInit = false;
 
-            $scope.submit = function(){
-                $scope._low = $scope.low ? Math.round($scope.low/1000) : '';
-                $scope._high = $scope.high ? Math.round($scope.high/1000) : '';
+            $scope.submit = function() {
+                $scope._low = $scope.low ? Math.round($scope.low / 1000) : '';
+                $scope._high = $scope.high ? Math.round($scope.high / 1000) : '';
 
                 if (!$scope.low && !$scope.high) {
                     $scope.item[$scope.attribute.Attribute] = "";
@@ -32,7 +30,7 @@ angular.module("designModule")
                 }
             }
 
-            $scope.$watch("item", function () {
+            $scope.$watch("item", function() {
                 if (typeof $scope.item === "undefined") {
                     return false;
                 }
@@ -44,8 +42,8 @@ angular.module("designModule")
                 var regExp = new RegExp("(\\d*)\\.\\.(\\d*)$", "i");
                 var values = value.match(regExp);
                 if (null !== values) {
-                    $scope.low = new Date(values[1]*1000);
-                    $scope.high = new Date(values[2]*1000);
+                    $scope.low = new Date(values[1] * 1000);
+                    $scope.high = new Date(values[2] * 1000);
                     isInit = true;
                 }
 
@@ -56,3 +54,4 @@ angular.module("designModule")
         }]
     }
 }]);
+
