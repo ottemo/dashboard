85e6b49dbeb10349cf2ab89785a34312c605a32f
Adam Knox <adam.w.knox@gmail.com>
2015-10-19 09:22:06 -0400

    [#95446004] route class directive

---
+++ app/scripts/design/directives/otRouteClass.js
@@ -0,0 +1,48 @@
+angular.module("designModule")
+
+.directive('otRouteClass', [function() {
+    return {
+        restrict: 'A',
+        link: function($scope, elem, iAttrs, controller) {
+            var cssPrefix = 'route__';
+            var routeRegex = new RegExp(cssPrefix + ".*", "g");
+
+            $scope.$on('$routeChangeSuccess', function(e, current, previous) {
+                // console.log('current', current);
+
+                // Remove all classes that this module applies
+                elem.removeClass(function(i, css) {
+                    return (css.match(routeRegex) || []).join(' ');
+                });
+
+                // Adding Classes
+                // NOTE: there is a load initially that doesn't have route info
+                if (current.$$route) {
+
+                    // controller class
+                    if (current.$$route.controller) {
+                        var ctrl = current.$$route.controller
+                        ctrl = ctrl.toLowerCase();
+                        ctrl = ctrl.replace('controller', '');
+
+                        var newClass = cssPrefix + 'ctrl--';
+                        newClass += ctrl;
+                        elem.addClass(newClass);
+                    }
+
+                    // template class
+                    if (current.$$route.templateUrl) {
+                        var url = current.$$route.templateUrl
+                        url = url.replace('/themes/views/', ''); // strip the dir
+                        url = url.replace('.html', '') // strip the extension
+                        url = url.replace('/', '-'); // replace the slashes
+
+                        var newClass = cssPrefix + 'tmpl--' + url;
+                        elem.addClass(newClass);
+                    }
+                }
+            });
+        }
+    };
+}]);
+
