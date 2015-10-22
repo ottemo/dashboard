ebf4887103311c8113d0c8259425a325deca7ec6
Adam Knox <adam.w.knox@gmail.com>
2015-10-22 09:28:35 -0400

    formatting, and adding use strict var

--- config/config.js
+++ config/config.js
@@ -1,18 +1,16 @@
-"use strict";
+"@@useStrict";
 
 angular.appConfig = {
-  "general.app.foundation_url": "@@apiUrl",
-  "general.app.media_path": "@@mediaPath",
-  "general.app.item_per_page": @@itemsPerPage
+    "general.app.foundation_url": "@@apiUrl",
+    "general.app.media_path": "@@mediaPath",
+    "general.app.item_per_page": @@itemsPerPage
 };
 
-angular.appConfigValue = function (valueName) {
-    if (typeof angular.appConfig[valueName] !== "undefined")
-        {
-            return angular.appConfig[valueName];
-        }
-    else
-        {
-            return "";
-        }
+angular.appConfigValue = function(valueName) {
+    if (typeof angular.appConfig[valueName] !== "undefined") {
+        return angular.appConfig[valueName];
+    } else {
+        return "";
+    }
 };
+
