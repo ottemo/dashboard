091612d5d5c1a7904b4b6233941ea44985ba2586
Adam Knox <adam.w.knox@gmail.com>
2016-03-15 14:38:43 -0400

    additional vet, indentation

--- src/app/order/list.controller.js
+++ src/app/order/list.controller.js
@@ -1,146 +1,158 @@
 angular.module('orderModule')
 
 .controller('orderListController', [
-'$scope',
-'$location',
-'$dashboardListService',
-'$orderApiService',
-function (
-    $scope,
-    $location,
-    DashboardListService,
-    $orderApiService
-) {
+    '$scope',
+    '$location',
+    '$dashboardListService',
+    '$orderApiService',
+    function(
+        $scope,
+        $location,
+        DashboardListService,
+        $orderApiService
+    ) {
 
+        var serviceList = new DashboardListService();
+        var showColumns = {
+            _id: {
+                type: 'select-link',
+                label: 'Order ID',
+                filter: 'text'
+            },
+            status: {},
+            customer_email: {},
+            customer_name: {},
+            grand_total: {
+                label: 'Total',
+                filter: 'range',
+                type: 'price'
+            },
+            created_at: {
+                label: 'Date',
+                type: 'date'
+            }
+        };
 
-    var serviceList = new DashboardListService();
-    var showColumns = {
-        _id : {type: 'select-link', label : 'Order ID', filter: 'text'},
-        status : {},
-        customer_email : {},
-        customer_name : {},
-        grand_total : {label : 'Total', filter : 'range', type : 'price'},
-        created_at : {label : 'Date', type : 'date'}
-    };
+        var searchDefaults = {
+            status: 'pending,new',
+            sort: '^created_at',
+            limit: '0,50',
+        };
 
-    var searchDefaults = {
-        status: 'pending,new',
-        sort: '^created_at',
-        limit: '0,50',
-    };
+        // REFACTOR: I only want to work with the selected ids
+        // instead of an object of ids where the value was the selected state
+        $scope.selectedIds = [];
+        $scope.idsSelectedRows = {};
 
-    // REFACTOR: I only want to work with the selected ids
-    // instead of an object of ids where the value was the selected state
-    $scope.selectedIds = [];
-    $scope.idsSelectedRows = {};
+        $scope.actions = {
+            isOpen: false,
+            export: angular.appConfigValue('general.app.foundation_url') + '/export/quickbooks',
+            printUrl: printUrl,
+            packingSlipUrl: packingSlipUrl,
+        };
 
-    $scope.actions = {
-        isOpen: false,
-        export: angular.appConfigValue('general.app.foundation_url') +'/export/quickbooks',
-        printUrl: printUrl,
-        packingSlipUrl: packingSlipUrl,
-    };
+        $scope.tabs = {
+            isActive: isTabActive,
+            setStatus: setStatusFilter,
+        };
 
-    $scope.tabs = {
-        isActive: isTabActive,
-        setStatus: setStatusFilter,
-    };
+        $scope.select = select;
+        $scope.create = create;
 
-    $scope.select = select;
-    $scope.create = create;
+        activate();
 
-    activate();
+        ///////////////////////////////////////
 
-    ///////////////////////////////////////
+        function activate() {
+            $scope.$watch('idsSelectedRows', function(newVal, oldVal) {
+                var ids = [];
+                angular.forEach($scope.idsSelectedRows, function(active, id) {
+                    if (active) {
+                        ids.push(id);
+                    }
+                });
+                $scope.selectedIds = ids;
+            }, true);
 
-    function activate() {
-        $scope.$watch('idsSelectedRows', function(newVal, oldVal){
-            var ids = [];
-            angular.forEach($scope.idsSelectedRows, function(active, id) {
-                if (active) {
-                    ids.push(id);
+            setSearchDefaults();
+
+            getOrderCount();
+            getAttributeList();
+        }
+
+        function setSearchDefaults() {
+            if (JSON.stringify({}) === JSON.stringify($location.search())) {
+                $location.search(searchDefaults).replace();
+            }
+        }
+
+        function getOrdersList() {
+            var params = $location.search();
+            params.extra = serviceList.getExtraFields();
+
+            $orderApiService.orderList(params).$promise.then(
+                function(response) {
+                    var result = response.result || [];
+                    $scope.orders = serviceList.getList(result);
                 }
-            });
-            $scope.selectedIds = ids;
-        }, true);
+            );
+        }
 
-        setSearchDefaults();
+        function getOrderCount() {
+            $orderApiService.getCount($location.search(), {}).$promise.then(
+                function(response) {
+                    if (response.error === null) {
+                        $scope.count = response.result;
+                    } else {
+                        $scope.count = 0;
+                    }
+                }
+            );
+        }
 
-        getOrderCount();
-        getAttributeList();
-    }
+        function getAttributeList() {
+            $orderApiService.getAttributes().$promise.then(
+                function(response) {
+                    var result = response.result || [];
+                    serviceList.init('orders');
+                    $scope.attributes = result;
+                    serviceList.setAttributes($scope.attributes);
+                    $scope.fields = serviceList.getFields(showColumns);
+                    getOrdersList();
+                }
+            );
+        }
 
-    function setSearchDefaults() {
-        if (JSON.stringify({}) === JSON.stringify($location.search())) {
-            $location.search(searchDefaults).replace();
+        function select(id) {
+            $location.path('/orders/' + id).search('');
+        }
+
+        function create() {
+            $location.path('/orders/new');
+        }
+
+        function hasSelectedRows() {
+            return $scope.selectedIds.length;
+        }
+
+        function printUrl() {
+            return '/orders/print?ids=' + $scope.selectedIds.join(',');
+        }
+
+        function packingSlipUrl() {
+            return '/orders/print?price=0&ids=' + $scope.selectedIds.join(',');
+        }
+
+        function isTabActive(compareStatus) {
+            var search = $location.search();
+            return compareStatus === search.status;
+        }
+
+        function setStatusFilter(newStatus) {
+            // Use the default search params as a starting point
+            searchDefaults.status = newStatus;
+            $location.search(searchDefaults);
         }
     }
+]);
 
-    function getOrdersList() {
-        var params = $location.search();
-        params.extra = serviceList.getExtraFields();
-
-        $orderApiService.orderList(params).$promise.then(
-            function (response) {
-                var result = response.result || [];
-                $scope.orders = serviceList.getList(result);
-            }
-        );
-    }
-
-     function getOrderCount() {
-        $orderApiService.getCount($location.search(), {}).$promise.then(
-            function (response) {
-                if (response.error === null) {
-                    $scope.count = response.result;
-                } else {
-                    $scope.count = 0;
-                }
-            }
-        );
-    }
-
-     function getAttributeList() {
-        $orderApiService.getAttributes().$promise.then(
-            function (response) {
-                var result = response.result || [];
-                serviceList.init('orders');
-                $scope.attributes = result;
-                serviceList.setAttributes($scope.attributes);
-                $scope.fields = serviceList.getFields(showColumns);
-                getOrdersList();
-            }
-        );
-    }
-
-    function select(id) {
-        $location.path('/orders/' + id).search('');
-    }
-
-    function create() {
-        $location.path('/orders/new');
-    }
-
-    function hasSelectedRows() {
-        return $scope.selectedIds.length;
-    }
-
-    function printUrl() {
-        return '/orders/print?ids=' + $scope.selectedIds.join(',');
-    }
-
-    function packingSlipUrl() {
-        return '/orders/print?price=0&ids=' + $scope.selectedIds.join(',');
-    }
-
-    function isTabActive(compareStatus) {
-        var search = $location.search();
-        return compareStatus === search.status;
-    }
-
-    function setStatusFilter(newStatus) {
-        // Use the default search params as a starting point
-        searchDefaults.status = newStatus;
-        $location.search(searchDefaults);
-    }
-}]);
