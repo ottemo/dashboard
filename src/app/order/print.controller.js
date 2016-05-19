c4dafe4e8c0acf1cadd72524518296fe75da2c79
Adam Knox <adam.w.knox@gmail.com>
2016-05-19 15:05:23 -0400

    print orders in the order in which they were selected

--- src/app/order/print.controller.js
+++ src/app/order/print.controller.js
@@ -10,7 +10,7 @@ angular.module('orderModule')
     function($scope, $location, $q, $timeout, orderApiService, cmsApiService) {
 
         $scope.options = {
-            showPrice: 0
+            showPrice: showPrice()
         };
         $scope.orders = [];
 
@@ -28,35 +28,28 @@ angular.module('orderModule')
         function activate() {
             var ids = $location.search().ids.split(',');
 
-            fetchOrders(ids);
-            fetchCmsHeader();
-            showPrice();
+            var o = fetchOrders(ids);
+            var c = fetchCmsHeader();
+            $q.all([o,c]).then(window.print);
+
         }
 
         // REFACTOR: We should just be making a single request to the server
         // http://api.ottemo.io/orders?_id=562125eb30dd91015200003d,5621252c30dd91015200001f
         // but right now the response from /orders is different than /order/:id
         function fetchOrders(ids) {
-            var allPromises = [];
-            angular.forEach(ids, function(id) {
-                var promise = orderApiService.getOrder({
-                        'orderID': id
-                    }).$promise
-                    .then(function(resp) {
-                        $scope.orders.push(resp.result);
-                    });
-
-                allPromises.push(promise);
+            var allPromises = ids.map(function(id) {
+                var params = { 'orderID': id };
+                return orderApiService.getOrder(params).$promise;
             });
 
             // Wait for all requests to complete
-            $q.all(allPromises)
-                .then(function( /*results*/ ) {
+            return $q.all(allPromises).then(function(results) {
+                    var orders = results.map(function(resp){
+                        return resp.result;
+                    });
 
-                    // Trigger the print dialog once we've had time to render
-                    $timeout(function() {
-                        window.print();
-                    }, 1000);
+                    $scope.orders = orders;
                 });
         }
 
@@ -71,7 +64,7 @@ angular.module('orderModule')
                 'extra': 'content',
             };
 
-            cmsApiService.blockList(params).$promise
+            return cmsApiService.blockList(params).$promise
                 .then(function(resp) {
                     // Normalize response
                     if (resp.result) {
@@ -98,7 +91,7 @@ angular.module('orderModule')
                 showPrice = parseInt($location.search().price, 10);
             }
 
-            $scope.options.showPrice = showPrice;
+            return showPrice;
         }
     }
 ]);
