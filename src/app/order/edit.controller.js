9dbabb7b7aea55b50f32822dbd3fc690d0ba7fbe
Dimitry <dglushenkov@speroteck.com>
2016-06-29 18:08:11 +0300

    Fix bug with product options on orders printing page (#300)
    * [#120193237] show option labels in orders printing
    
    * restore commented lines

--- src/app/order/edit.controller.js
+++ src/app/order/edit.controller.js
@@ -34,6 +34,8 @@ function ($scope, $routeParams, $location, $q, orderApiService, dashboardUtilsSe
         };
     };
 
+    $scope.getOptionValueLabel = dashboardUtilsService.getOptionValueLabel;
+
     /**
      * Current selected order
      *
@@ -95,27 +97,6 @@ function ($scope, $routeParams, $location, $q, orderApiService, dashboardUtilsSe
             }
         );
     }
-
-    /**
-     * Returns a label of selected product option
-     * @param {Object} option
-     * @returns {string}
-     */
-    $scope.getOptionLabel = function(option) {
-        // If option is a field return option value
-        if (option.type === 'field') {
-            return option.value;
-
-        // If option is a multi-select return a list of labels
-        } else if (option.type === 'multi_select') {
-            return _.map(option.options, 'label').join(', ');
-
-        // In other cases (select, radio, subscription select)
-        } else {
-            var childOptions = option.options;
-            return childOptions[option.value].label;
-        }
-    };
 
     /**
      * Event handler to save the order data.
