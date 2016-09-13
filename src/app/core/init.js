fdeff6e78787c2e51519e47a304cf0186465a24d
Dimitry <dglushenkov@speroteck.com>
2016-09-13 15:59:45 +0300

    Revert "Vs 128340699 add to ability insert cms images into cms page"

--- src/app/core/init.js
+++ src/app/core/init.js
@@ -1,42 +1,8 @@
-angular.module('coreModule',['textAngular'])
+angular.module("coreModule",[])
 
-.constant('MEDIA_BASE_PATH', angular.appConfigValue('general.app.media_path'))
-.constant('PRODUCT_DEFAULT_IMG', 'placeholder.png')
+.constant("MEDIA_BASE_PATH", angular.appConfigValue("general.app.media_path"))
+.constant("PRODUCT_DEFAULT_IMG", "placeholder.png")
 
 // non-angularized libs
-.constant('moment', window.moment)
-.constant('_', window._)
-
-.config(function ($provide) {
-
-    $provide.decorator('taOptions', ['taRegisterTool', '$delegate', '$uibModal', function (taRegisterTool, taOptions, $uibModal) {
-        taRegisterTool('uploadImage', {
-            buttontext: 'Insert Image',
-            tooltiptext: 'Upload image',
-            iconclass: "fa fa-image",
-            action: function (deferred,restoreSelection) {
-                $uibModal.open({
-                    controller: 'cmsPageUploadController',
-                    templateUrl: "/views/cms/page/upload.html",
-                    size: 'lg'
-                }).result.then(
-                    function (result) {
-                        restoreSelection();
-                        document.execCommand('insertImage', true, result);
-                        deferred.resolve();
-                    },
-                    function () {
-                        deferred.resolve();
-                    }
-                );
-                return false;
-            }
-        });
-        taOptions.toolbar[3].push('uploadImage');
-        return taOptions;
-    }]);
-});
-
-
-
-
+.constant("moment", window.moment)
+.constant("_", window._);
