4c4fa458edd8f47252c1d969e8a96bfe61ced2e0
root <root@awork.localdomain>
2017-03-23 13:47:15 +0200

    allow to define version during build

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -2,14 +2,30 @@
 
 # build and push dashdashboard image to registry
 
+for i in "$@"
+do
+case $i in
+    version=*)
+    version="${i#*=}"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
 MYDIR=$(cd `dirname ${BASH_SOURCE[0]}` && pwd)
 DASHREPO="$MYDIR/.."
 cd $DASHREPO
 
-date=$(date +%Y%m%d-%H%M%S)
-branch=$(git branch| awk '{print $2}')
-branch=$(echo $branch|sed 's/ /_/g')
-IMAGE="gcr.io/ottemo-kube/dashboard:${branch}-${date}"
+if ! [ -n "$version" ] ; then
+  date=$(date +%Y%m%d-%H%M%S)
+  IMAGE="gcr.io/ottemo-kube/dashboard:${date}"
+else
+  IMAGE="gcr.io/ottemo-kube/dashboard:$version"
+fi
+echo "use $IMAGE as image name"
 
 echo "build alpine based dashboard container"
 docker build -t $IMAGE .
